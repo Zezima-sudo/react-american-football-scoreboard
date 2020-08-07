159716c1ae18a0f626357d3e50a26f101786be95
Eric <ericmichaelrice@protonmail.com>
2020-08-06 23:59:22 -0700

    MVP + Working on stretch

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
+import QuarterButtons from './quarterButtons'
 
 const BottomRow = () => {
   return (
     <div className="bottomRow">
+      
       <div className="down">
+       
         <h3 className="down__title">Down</h3>
         <div className="down__value">3</div>
       </div>
       <div className="toGo">
+        
         <h3 className="toGo__title">To Go</h3>
+        
         <div className="toGo__value">7</div>
       </div>
       <div className="ballOn">
@@ -20,7 +25,10 @@ const BottomRow = () => {
         <h3 className="quarter__title">Quarter</h3>
         <div className="quarter__value">4</div>
       </div>
+   
     </div>
+   
+    
   );
 };
 
---
+++ src/quarterButtons.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import { Button } from 'reactstrap'
+
+function QuarterButtons() {
+return (
+   <ul>
+        <Button>Down</Button>
+            <Button>To Go</Button>
+             <Button>Ball On</Button>
+              <Button>Quarter</Button>
+              </ul>
+   
+)
+}
+
+
+export default QuarterButtons
